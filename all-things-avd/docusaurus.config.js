b9ca53e1b0e7e72cf4163bff8063c98f2ea399d8
Jamie Moxon <jamie.moxon@shapingcloud.com>
2024-06-10 16:48:20 +0100

    adding Copyright

--- all-things-avd/docusaurus.config.js
+++ all-things-avd/docusaurus.config.js
@@ -175,7 +175,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} All Things AVD.`,
       },
       prism: {
         theme: prismThemes.github,
